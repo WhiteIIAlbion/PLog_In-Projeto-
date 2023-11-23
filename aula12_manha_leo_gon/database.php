6fbd23c707f3a55ce449d92d8a173da1dcd4da43
WhiteIIAlbion <117367034+WhiteIIAlbion@users.noreply.github.com>
2023-11-23 12:00:47 -0300

    Add files via upload

---
+++ aula12_manha_leo_gon/database.php
@@ -0,0 +1,15 @@
+<?php
+
+//Criando lista de dados
+$database = array(
+    0 => [
+        "user" => "admin",
+        "pass" => "1234",
+        "photo" => "/aula12_manha_leo_gon/assets/img/profile/profile1.jpeg"
+    ],
+    1 => [
+        "user" => "donini",
+        "pass" => "@senha",
+        "photo" => "/aula12_manha_leo_gon/assets/img/profile/profile2.png"
+    ],
+);
